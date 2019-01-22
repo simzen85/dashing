977681f24f945eba989078cd1e3da39ea59c23c4
Sim <simzen85@gmail.com>
2019-01-22 11:42:01 +0700

    Downgrade Sprockets

--- lib/dashing/app.rb
+++ lib/dashing/app.rb
@@ -160,8 +160,7 @@ def first_dashboard
 end
 
 def tilt_html_engines
-  mapping = Tilt::Mapping.new
-  mapping.select do |_, engines|
+  Tilt.mappings.select do |_, engines|
     default_mime_type = engines.first.default_mime_type
     default_mime_type.nil? || default_mime_type == 'text/html'
   end
